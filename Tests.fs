622d44736bc71ce7723fd9e5bd57c610e8024f81
Daniel Oliver <danieloliversoftware@gmail.com>
2018-06-10 10:32:51 -0400

    replace test

--- Tests.fs
+++ Tests.fs
@@ -4,10 +4,10 @@ open Xunit
 open canopy.parallell
 
 [<Fact>]
-let ``Hightlight Google Image Test`` () =
+let ``Hightlight Olivercoding Image Test`` () =
     use browser = functions.start canopy.types.Chrome
-    functions.url "https://www.google.com" browser
-    functions.highlight "#hplogo" browser
+    functions.url "https://www.olivercoding.com/" browser
+    functions.highlight "#mainLogoDiv" browser
     functions.sleep 5
     Assert.True(true)
 
